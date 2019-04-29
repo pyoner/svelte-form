a3be9338a3b3e510e20ff0068ec5751be8c8aa3c
Jungle <devex.soft@gmail.com>
2019-04-29 13:34:29 +0500

    build(deps, rollup): Add rollup-plugin-svelte, @pyoner/svelte-ts-preprocess. Change rollup.config.ts

--- packages/lib/rollup.config.ts
+++ packages/lib/rollup.config.ts
@@ -5,6 +5,21 @@ import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
 
+import svelte from 'rollup-plugin-svelte'
+import { preprocess, createEnv, readConfigFile } from '@pyoner/svelte-ts-preprocess'
+
+const production = !process.env.ROLLUP_WATCH
+
+const env = createEnv()
+const compilerOptions = readConfigFile(env)
+const opts = {
+  env,
+  compilerOptions: {
+    ...compilerOptions,
+    allowNonTsExtensions: true
+  }
+}
+
 const pkg = require('./package.json')
 
 const libraryName = 'svelte-form'
@@ -13,26 +28,36 @@ export default {
   input: `src/${libraryName}.ts`,
   output: [
     { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true }
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
   watch: {
-    include: 'src/**',
+    include: 'src/**'
   },
   plugins: [
+    svelte({
+      // enable run-time checks when not in production
+      dev: !production,
+      // we'll extract any component CSS out into
+      // a separate file — better for performance
+      css: css => {
+        css.write('public/bundle.css')
+      },
+      preprocess: preprocess(opts)
+    }),
     // Allow json resolution
     json(),
-    // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
+    // Compile TypeScript files
+    typescript({ useTsconfigDeclarationDir: true }),
 
     // Resolve source maps to the original source
-    sourceMaps(),
-  ],
+    sourceMaps()
+  ]
 }
