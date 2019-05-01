fb24f8b195dfe77dd3692f1f24464ba2c792696b
Jungle <devex.soft@gmail.com>
2019-05-01 09:33:11 +0500

    feat(field): Add NumberField.svelte

---
+++ packages/lib/src/fields/NumberField.svelte
@@ -0,0 +1,15 @@
+<svelte:component this={wrapper} {schema} {error}>
+  <input type="number" {value} />
+</svelte:component>
+
+<script lang="ts">
+  import { JSONSchema } from 'json-schema-typed'
+  import { ajv } from 'ajv'
+
+  import GenericField from './GenericField.svelte'
+
+  export let value: number
+  export let error: ajv.ErrorObject
+  export let schema: JSONSchema
+  export let wrapper = GenericField
+</script>
