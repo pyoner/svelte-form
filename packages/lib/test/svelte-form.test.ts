4ee170cf5403c4d8d212821f30363d3f79d39741
Jungle <devex.soft@gmail.com>
2019-05-01 10:13:37 +0500

    test(field): Add a test for GenericField.svelte

--- packages/lib/test/svelte-form.test.ts
+++ packages/lib/test/svelte-form.test.ts
@@ -1,14 +1,7 @@
-import DummyClass from "../src/svelte-form"
+import { GenericField } from '../src/svelte-form'
 
-/**
- * Dummy test
- */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
-  })
-
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+describe('GenericField test', () => {
+  it('exists', () => {
+    expect(GenericField).toBeTruthy()
   })
 })
