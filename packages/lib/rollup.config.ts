3ea095ce94d2c5359a8e9362b9bd462900253465
Jungle <devex.soft@gmail.com>
2019-09-06 11:03:42 +0500

    build(deps): Fix dev deps

--- packages/lib/rollup.config.ts
+++ packages/lib/rollup.config.ts
@@ -1,7 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
+import camelCase from 'camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
 
