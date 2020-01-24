34f190e4dfd794b51cf30afa4d1c65e4a89519fb
Jungle <devex.soft@gmail.com>
2020-01-24 08:39:47 +0500

    feat(components): Add description, errors to Wrapper

--- packages/lib/src/components/Wrapper.svelte
+++ packages/lib/src/components/Wrapper.svelte
@@ -16,7 +16,18 @@
   {#if schema.type == 'object' || schema.type == 'array'}
     <fieldset>
       <legend>{schema.title}</legend>
+
+      {#if schema.description}
+        <div class="description">{schema.description}</div>
+      {/if}
+
       <slot>A field is not implemented</slot>
+
+      {#if errors && errors.length}
+        {#each errors as error}
+          <div class="error">{error.message}</div>
+        {/each}
+      {/if}
     </fieldset>
   {:else}
     <div class="field">
