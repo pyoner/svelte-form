075e8a580916859920a2f761888eaef5d41dcb71
Jungle <devex.soft@gmail.com>
2020-07-06 14:23:57 +0500

    build(rollup): Update rollup, @rollup/plugin-commonjs

--- packages/app/rollup.config.js
+++ packages/app/rollup.config.js
@@ -1,6 +1,6 @@
 import svelte from "rollup-plugin-svelte";
 import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
+import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
--- packages/lib/rollup.config.ts
+++ packages/lib/rollup.config.ts
@@ -1,5 +1,5 @@
 import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
+import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 
 import sourceMaps from "rollup-plugin-sourcemaps";
