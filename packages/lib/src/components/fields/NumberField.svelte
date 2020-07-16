1b94b948b0536ff06801a020f7e65b681f6b00e0
Askar Yusupov <devex.soft@gmail.com>
2020-07-16 17:32:13 +0500

    feat(wrap.svelte): add Wrap.svelte component

--- packages/lib/src/components/fields/NumberField.svelte
+++ packages/lib/src/components/fields/NumberField.svelte
@@ -1,26 +1,29 @@
 <script lang="ts">
-  import { createProps, defaultValue, getComponent, getComponentProps } from '../../helpers'
+  import { createProps, defaultValue, getComponent, getComponentProps } from "../../helpers";
+  import Wrap from "../helpers/Wrap.svelte";
 
-  type T = number
-  const p = createProps<T>()
-  export let value = p.value
-  export let errors = p.errors
-  export let schema = p.schema
-  export let components = p.components
-  export let props = p.props
+  type T = number;
+  const p = createProps<T>();
+  export let value = p.value;
+  export let errors = p.errors;
+  export let schema = p.schema;
+  export let components = p.components;
+  export let props = p.props;
 
   /* recalc a default value */
   $: if (schema && value == null) {
-    value = defaultValue<T>(schema, value)
+    value = defaultValue<T>(schema, value);
   }
 </script>
 
 {#if schema && components}
-  <svelte:component
-    this={getComponent(components.wrapper)}
-    {...getComponentProps(components.wrapper)}
-    {schema}
-    {errors}>
-    <input {...props} type="number" step={props.step || 'any'} bind:value />
-  </svelte:component>
+  <Wrap {schema} {errors} component={components.wrapper}>
+    <svelte:component
+      this={getComponent(components.wrapper)}
+      {...getComponentProps(components.wrapper)}
+      {schema}
+      {errors}>
+      <input {...props} type="number" step={props.step || 'any'} bind:value />
+    </svelte:component>
+  </Wrap>
 {/if}
---
+++ packages/lib/src/components/helpers/Wrap.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { getComponent, getComponentProps } from "../../helpers";
+  import type { SvelteSchema, TSvelteComponent } from "../../types";
+
+  export let errors: Error[] | null = null;
+  export let schema: SvelteSchema;
+  export let component: TSvelteComponent;
+</script>
+
+<svelte:component
+  this={getComponent(component)}
+  {...getComponentProps(component)}
+  {schema}
+  {errors}>
+  <slot />
+</svelte:component>
